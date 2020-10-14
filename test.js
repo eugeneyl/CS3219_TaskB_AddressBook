add2be412f381db20a2125da2770afd38009b649
Eugene Yong <eugeneyl96@gmail.com>
2020-10-15 01:16:53 +0800

    Add: test for put command

--- test.js
+++ test.js
@@ -67,8 +67,8 @@ describe('contact', () => {
     })
 
     //Test the /GET/:id route with correct id
-    describe('/GET contact', () => {
-        it('it should GET the contact with the correspoding', (done) => {
+    describe('/GET/:id contact', () => {
+        it('it should GET the contact with the correspoding id', (done) => {
             var temp = new Contact();
             temp.name = contacts[0].name
             temp.gender = contacts[0].gender;
@@ -91,8 +91,8 @@ describe('contact', () => {
     })
 
     //Test the /GET/:id route with incorrect id
-    describe('/GET contact', () => {
-        it('it should GET the contact with the correspoding', (done) => {
+    describe('/GET/:id contact', () => {
+        it('it should not GET any contact', (done) => {
             var temp = new Contact();
             temp.name = contacts[0].name
             temp.gender = contacts[0].gender;
@@ -195,5 +195,80 @@ describe('contact', () => {
                 })
         })
     }) 
+
+    /**
+     * /PUT route
+     */
+    //Test the /PUT/:id route with correct id and valid fields
+    describe('/PUT/:id contact', () => {
+        it('it should edit the contact with the correspoding id and replace the email field with new email', (done) => {
+            var temp = new Contact();
+            temp.name = contacts[0].name
+            temp.gender = contacts[0].gender;
+            temp.email = contacts[0].email;
+            temp.phone = contacts[0].phone;
+            temp.save((err, contact) => {
+                var newEmail = "new_email@gmail.com"
+                chai
+                    .request(server)
+                    .put("/api/contacts/" + contact.id)
+                    .send({email: newEmail})
+                    .end((err, res) => {
+                        res.body.data.should.be.a('object');
+                        res.body.data.should.have.property('name').eql(temp.name)
+                        res.body.data.should.have.property('gender').eql(temp.gender)
+                        res.body.data.should.have.property('phone').eql(temp.phone)
+                        res.body.data.should.have.property('email').eql(newEmail)
+                        done()
+                    })
+            })
+        })
+    })
+
+    //Test the /PUT/:id route with correct id and no fields
+    describe('/PUT/:id contact', () => {
+        it('it should not edit any of the fields of the contact with the corresponding id', (done) => {
+            var temp = new Contact();
+            temp.name = contacts[0].name
+            temp.gender = contacts[0].gender;
+            temp.email = contacts[0].email;
+            temp.phone = contacts[0].phone;
+            temp.save((err, contact) => {
+                chai
+                    .request(server)
+                    .put("/api/contacts/" + contact.id)
+                    .end((err, res) => {
+                        res.body.data.should.be.a('object');
+                        res.body.data.should.have.property('name').eql(temp.name)
+                        res.body.data.should.have.property('gender').eql(temp.gender)
+                        res.body.data.should.have.property('phone').eql(temp.phone)
+                        res.body.data.should.have.property('email').eql(temp.email)
+                        done()
+                    })
+            })
+        })
+    })
+
+    //Test the /PUT/:id route with incorrect id
+    describe('/PUT/:id contact', () => {
+        it('it should show that the no contact can be found', (done) => {
+            var temp = new Contact();
+            temp.name = contacts[0].name
+            temp.gender = contacts[0].gender;
+            temp.email = contacts[0].email;
+            temp.phone = contacts[0].phone;
+            temp.save((err, contact) => {
+                chai
+                    .request(server)
+                    .put("/api/contacts/" + "123")
+                    .end((err, res) => {
+                        res.body.should.have.property("status").eql("error")
+                        res.body.should.have.property("message").eql("Contact cannot be found.")
+                        done()
+                    })
+            })
+        })
+    })
+    
 })
 
