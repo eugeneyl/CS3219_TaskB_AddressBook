8c2f3ce00831af4a024b517cde9577f02a423111
Eugene Yong <eugeneyl96@gmail.com>
2020-10-15 00:48:24 +0800

    Add: Test for post route

--- test.js
+++ test.js
@@ -66,6 +66,88 @@ describe('contact', () => {
         })
     })
 
+    /**
+     * /POST route
+     */
 
+     //Test the /POST route with no fields at all
+     describe('/POST contact', () => {
+        it('it should not POST contact with no fields', (done) => {
+
+            chai
+                .request(server)
+                .post("/api/contacts")
+                .end((err, res) => {
+                    res.body.should.have.property("status").eql("error")
+                    res.body.should.have.property("message").eql("Please input a name for your contact.")
+                    done()
+                })
+        })
+    }) 
+
+    //Test the /POST route with no name fields
+    describe('/POST contact', () => {
+        it('it should not POST contact with no name fields', (done) => {
+            var temp = {
+                gender: "Male",
+                email: "jd@gmail.com",
+                phone: "91122334"
+            }
+
+            chai
+                .request(server)
+                .post("/api/contacts")
+                .send(temp)
+                .end((err, res) => {
+                    res.body.should.have.property("status").eql("error")
+                    res.body.should.have.property("message").eql("Please input a name for your contact.")
+                    done()
+                })
+        })
+    }) 
+
+    //Test the /POST route with complete data
+    describe('/POST contact', () => {
+        it('it should POST contact successfully with all data fields present', (done) => {
+            var temp = contacts[0]
+
+            chai
+                .request(server)
+                .post("/api/contacts")
+                .send(temp)
+                .end((err, res) => {
+                    res.body.data.should.be.a('object')
+                    res.body.data.should.have.property('name').eql(temp.name)
+                    res.body.data.should.have.property('gender').eql(temp.gender)
+                    res.body.data.should.have.property('phone').eql(temp.phone)
+                    res.body.data.should.have.property('email').eql(temp.email)
+                    done()
+                })
+        })
+    })
+
+    //Test the /POST route with missing gender
+    describe('/POST contact', () => {
+        it('it should POST contact successfully with missing gender field', (done) => {
+            var temp = {
+                name: "John Doe",
+                email: "jd@gmail.com",
+                phone: "91122334"
+            }
+
+            chai
+                .request(server)
+                .post("/api/contacts")
+                .send(temp)
+                .end((err, res) => {
+                    res.body.data.should.be.a('object')
+                    res.body.data.should.not.have.property('gender')
+                    res.body.data.should.have.property('name').eql(temp.name)
+                    res.body.data.should.have.property('phone').eql(temp.phone)
+                    res.body.data.should.have.property('email').eql(temp.email)
+                    done()
+                })
+        })
+    }) 
 })
 
