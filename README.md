f23c0af31be9e3c95ed6045e8982d397a6265e2e
eugeneyl <46802650+eugeneyl@users.noreply.github.com>
2020-10-14 22:37:54 +0800

    Initial upload of files

---
+++ README.md
@@ -0,0 +1,42 @@
+To run backend with hot reload:
+
+```
+nodemon index
+```
+
+Start mongodb instance on MacOS
+
+sudo mongod --config /usr/local/etc/mongod.conf
+
+
+
+To run:
+
+1. Start mongodb. 
+
+```
+# For MacOS if installed using brew
+sudo mongod --config /usr/local/etc/mongod.conf
+```
+
+2. Install dependencies.
+
+```
+npm install
+```
+
+3. Run the backend server with hot reload.
+
+```
+npm start
+```
+
+
+
+API Endpoints:
+
+- `GET /api/contacts` list all contacts
+- `POST /api/contacts` create new contact
+- `GET /api/contacts/{id}` retrieve a single contact
+- `PUT /api/contacts/{id}` update a single contact
+- `DELETE /api/contacts/{id}` delete a single contact
---
+++ controllers/contactController.js
@@ -0,0 +1,101 @@
+// Import contact model
+var Contact = require('../models/contactModel');
+
+// Handle index actions
+exports.index = function (req, res) {
+    Contact.get(function (err, contacts) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err,
+            });
+        }
+        res.json({
+            status: "success",
+            message: "Contacts retrieved successfully",
+            data: contacts
+        });
+    });
+};
+
+// Handle create contact actions
+exports.new = function (req, res) {
+    var contact = new Contact();
+    contact.name = req.body.name;
+    contact.gender = req.body.gender;
+    contact.email = req.body.email;
+    contact.phone = req.body.phone;
+
+    //return error if name is not filled
+    if (!contact.name) {
+        res.json({
+            status: "error",
+            message: "Please input a name for your contact."
+        })
+    }
+
+    // save the contact and check for errors
+    contact.save(function (err) {
+        if (err) {
+            res.json({
+                status: "error",
+                message: err
+            });
+        }
+        res.json({
+            message: 'New contact created!',
+            data: contact
+        });
+    });
+};
+
+// Handle view contact info
+exports.view = function (req, res) {
+    Contact.findById(req.params.contact_id, function (err, contact) {
+        if (err)
+            res.send(err);
+        res.json({
+            message: 'Contact details loading..',
+            data: contact
+        });
+    });
+};
+
+// Handle update contact info
+exports.update = function (req, res) {
+    Contact.findById(req.params.contact_id, function (err1, contact) {
+        if (err1) {
+            res.send(err1);
+        }    
+        
+        contact.name = req.body.name ? req.body.name : contact.name;
+        contact.gender = req.body.gender ? req.body.gender : contact.gender;
+        contact.email = req.body.email ? req.body.email : contact.gender;
+        contact.phone = req.body.phone ? req.body.phone : contact.phone;
+        
+        // save the contact and check for errors
+        contact.save(function (err2) {
+            if (err2)
+                res.json(err2);
+            res.json({
+                message: 'Contact Info updated',
+                data: contact
+            });
+        });
+    });
+};
+
+// Handle delete contact
+exports.delete = function (req, res) {
+    Contact.remove({
+        _id: req.params.contact_id
+    }, function (err, contact) {
+        if (err) {
+            res.send(err);
+        }
+        res.json({
+            status: "success",
+            message: 'Contact deleted'
+        });
+    });
+};
