a675c0d117bceee9cd9f6f30256c0a1bdb8981f3
Eugene Yong <eugeneyl96@gmail.com>
2020-10-15 12:23:20 +0800

    Edit: remove test statement

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ app.use(bodyParser.json());
 
 var uri = process.env.MODE && process.env.MODE === "SERVERLESS" ? "mongodb+srv://cs3219-testuser:i4QjONtOfyJzQmoV@eugeneyl-cluster.lu3m4.mongodb.net/cs3219_addressbook?retryWrites=true&w=majority" : "mongodb://localhost/cs3219_addressbook"
 
-console.log(uri)
 // Connect to Mongoose and set connection variable
 mongoose.connect(uri, { useNewUrlParser: true});
 var db = mongoose.connection;
